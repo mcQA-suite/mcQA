c3dfec8a549dfa9f8d90353209c6746751cfd0d5
Taycir <taycir.yahmed@gmail.com>
2019-07-15 22:09:49 +0200

    setup

---
+++ setup.py
@@ -0,0 +1,23 @@
+import os
+import setuptools
+
+def read(file):
+    return open(os.path.join(os.path.dirname(__file__), file)).read()
+
+setuptools.setup(
+    name="mcqa",
+    version="0.0.1",
+    author="Taycir Yahmed",
+    author_email="taycir.yahmed@gmail.com",
+    description="Answering multiple choice questions with Language Models.",
+    long_description=read('README.md'),
+    long_description_content_type="text/markdown",
+    url="https://github.com/mcQA-suite/mcQA",
+    packages=setuptools.find_packages(),
+    install_requires=read('requirements.txt').split(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache License",
+        "Operating System :: OS Independent",
+    ],
+)
