f0e9bba12a1e23fef9daf25e04677fe585b91e02
Taycir Yahmed <taycir.yahmed@gmail.com>
2019-07-21 09:11:01 +0000

    debug tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -51,7 +51,7 @@ def mcqa_data():
 
 
 @pytest.fixture(scope="function")
-def mcqa_dataset(dummy_data_path):
+def mcqa_dataset(mcqa_data, dummy_data_path):
     dataset = mcqa_data.read(dummy_data_path,
                              is_training=True)
     yield dataset
