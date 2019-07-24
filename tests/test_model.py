a34fa751e4ef42d7d5f688178dcca649feb0e09e
Taycir Yahmed <taycir.yahmed@gmail.com>
2019-07-24 19:55:21 +0000

    predicttest

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,3 +1,4 @@
+import pytest
 from mcqa.models import Model
 from pytorch_transformers import BertForMultipleChoice
 
@@ -11,3 +12,21 @@ def test_fit(mcqa_dataset):
             num_train_epochs=1)
 
     assert isinstance(mdl.model, BertForMultipleChoice)
+
+
+@pytest.fixture(scope='session')
+def trained_model(mcqa_dataset):
+
+    mdl = Model(bert_model="bert-base-uncased",
+                device="cpu")
+
+    mdl.fit(mcqa_dataset,
+            train_batch_size=1,
+            num_train_epochs=1)
+
+    yield mdl
+
+
+def test_predict(mcqa_dataset, trained_model):
+    outputs = trained_model.predict(mcqa_dataset)
+    assert len(outputs) == len(mcqa_dataset)
